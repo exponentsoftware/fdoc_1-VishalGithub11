fc8715a66b6552d80e825ea9cece32767488b1e5
Vishal Modanwal <modanwalvishal@gmail.com>
2021-07-29 18:12:50 +0530

    1(d)

--- numberOfWords.js
+++ numberOfWords.js
@@ -1,7 +1,8 @@
 const sentence = `I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`;
 
 const countWords = (para) => {
-  console.log("length", para.split(" ").length);
+    const words = para.split(" ").filter((word) => word.length > 1);
+    console.log(words.length);
 };
 
 countWords(sentence);
